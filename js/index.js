d33f30fd12cf5b7193b6c12ed355586d584152ad
Andrii Kondysiuk <kondysiuka@gmail.com>
2021-11-11 15:32:18 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
-// async function getResponse() {
-//   let response = await fetch(
-//     "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
-//   );
-//   let content = await response.json();
-//   content = content.splice(0, 3);
-//   let list = document.querySelector(".posts");
-//   let key;
-//   for (key in content) {
-//     list.innerHTML += `
-//     <div>
-//     <h3> ${content[key].name}</h3>
-//     <p> ${content[key].price}</p>
-//     </div>
-//       `;
-//     console.log(content[key].name);
-//   }
-// }
+const url = "https://makeup-api.herokuapp.com/api/v1/products.json?brand=";
+const posts = document.querySelector("#app .posts");
 
-// getResponse();
+async function requestMakeupData(brand) {
+    if (typeof brand !== "string")
+        throw new Error("argument `brand` must be an string");
+    let response = await fetch(url + brand);
+    let products = await response.json();
+    products
+        .filter((product) => parseFloat(product.price) < 5)
+        .forEach((product) => {
+            let node = document.createElement("li");
+            let nodeHeader = document.createElement("h3");
+            nodeHeader.innerText = product.name;
+            node.append(nodeHeader);
+            let nodeText = document.createElement("p");
+            nodeText.innerText = product.price;
+            node.append(nodeText);
+            posts.append(node);
+        });
+}
+
+requestMakeupData("maybelline");
